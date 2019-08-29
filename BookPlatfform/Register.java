a1afae8cf3b007f9a6f28dc6a2dbcd888da41f9e
DI CHEN <dche0045@student.monash.edu>
2019-08-29 16:31:57 +1000

    Booking system java code

---
+++ BookPlatfform/Register.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Register here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Register
+{
+    // instance variables - replace the example below with your own
+    private int accountNum;
+    private String accountName;
+    private String 
+
+    /**
+     * Constructor for objects of class Register
+     */
+    public Register()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    public void actorRegister
+    {
+        
+    }
+}
